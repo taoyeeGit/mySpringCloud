6df24658e665c3b419defe714a469ff47068ca58
taoyeeGit <taoyee@139.com>
2022-09-11 21:04:29 +0800

    nacos+feign+RestTemplate:9000负载均衡-->9001、9002集群

--- alibaba-nacos-zf9001/src/main/java/com/taoyee/cloud/NacosZfMain9001.java
+++ alibaba-nacos-zf9001/src/main/java/com/taoyee/cloud/NacosZfMain9001.java
@@ -1,4 +1,4 @@
-package com.taoyee;
+package com.taoyee.cloud;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@Slf4j
 @EnableDiscoveryClient
+@Slf4j
 public class NacosZfMain9001 {
     public static void main(String[] args) {
         SpringApplication.run(NacosZfMain9001.class,args);
